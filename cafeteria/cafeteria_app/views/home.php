b318b57e6feeb4f21ddf56bfa432119d730e0386
ignaciovillar10 <ignaciovillaar@gmail.com>
2025-10-13 15:12:07 -0300

    Delete cafeteria/cafeteria_app directory

--- cafeteria/cafeteria_app/views/home.php
+++
@@ -1,4 +0,0 @@
-<?php require 'views/partials/header.php'; ?>
-<h1>Bienvenido a la Cafetería</h1>
-<p>Usa el menú para navegar entre Usuarios, Categorías, Productos y Pedidos.</p>
-<?php require 'views/partials/footer.php'; ?>
